d5bb70e8d6c9e927a12740a2034c40d523b048c2
wendwin <wendwin12@gmail.com>
2025-06-09 21:43:16 +0700

    membuat views search

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from search_engine import search_cosine, search_bm25
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/search/cosine', methods=['GET'])
+def search_cosine_route():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'error': 'Query is required'}), 400
+    result = search_cosine(query)
+    return jsonify(result)
+
+@app.route('/search/bm25', methods=['GET'])
+def search_bm25_route():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'error': 'Query is required'}), 400
+    result = search_bm25(query)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
